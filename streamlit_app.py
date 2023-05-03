b728c27451717008d5b082b91cbdb2b2098e00ff
abhinavkrishna15 <68146805+abhinavkrishna15@users.noreply.github.com>
2023-05-03 18:07:16 +0530

    Update streamlit_app.py
    added breakfast menu header and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title('Hi ! I am a Data Engineer')
-streamlit.header('Abhinav Krishna')
-streamlit.text('Abhinav Krishna')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
